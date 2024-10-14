b18363255eaf28a176b041fe729029e75f52fcbb
Scanood <wangb.edu@gmail.com>
2024-10-14 16:12:40 +0800

    doc: live demo

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,20 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    lib: {
-      entry: path.resolve('src/index.js'),
-      name: 'prefetch-router-link',
-      fileName: 'prefetch-router-link'
-    },
-    rollupOptions: {
-      external: ['vue', 'vue-router'],
-      output: {
-        globals: {
-          "vue": 'Vue',
-          "vue-router": "VueRouter"
-        },
-      },
-    },
-  }
+  // build: {
+  //   lib: {
+  //     entry: path.resolve('src/index.js'),
+  //     name: 'prefetch-router-link',
+  //     fileName: 'prefetch-router-link'
+  //   },
+  //   rollupOptions: {
+  //     external: ['vue', 'vue-router'],
+  //     output: {
+  //       globals: {
+  //         "vue": 'Vue',
+  //         "vue-router": "VueRouter"
+  //       },
+  //     },
+  //   },
+  // }
 })
